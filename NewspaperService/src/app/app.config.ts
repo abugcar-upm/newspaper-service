5ccb035b35a96a739cb03cf7b30775fdd560e9d5
aiden1614 <aiden.bugarin.carreira@gmail.com>
2026-09-21 11:52:24 +0200

    create project

---
+++ NewspaperService/src/app/app.config.ts
@@ -0,0 +1,16 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  importProvidersFrom,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    importProvidersFrom(NgbModule),
+  ],
+};
